28392a710aab8d1206d88e3a2618e9f5c016cfea
Drew Slattery <drewslatts@gmail.com>
2017-06-22 18:29:27 -0400

    feat(canvas):can now erase and change brush size

--- browser/components/Toolbar.js
+++ browser/components/Toolbar.js
@@ -1,22 +1,37 @@
 import React from 'react';
 
 export default function Toolbox(props){
-  let colors = ['#DE6161', '#3EB0F1', '#E174D0', '#F6F6E6',
-                '#EBC35B', '#BCE16E', '#4E53C8', '#3A2B3D'];
-  let size = '75px';
+  let colors = ['#DD4842', '#3EB8F1', '#C94DAB', '#614238',
+                '#F2D665', '#BCE16E', '#343AC5', '#211B22'];
+  let brushSizes = [2, 10, 25, 50, 70];
+  let swatchSize = '75px';
 
   return (
-    <div>
-    Colors:
-      { colors.map(color => <div
-        key={color}
-        onClick={props.changeColor}
-        style={{display: 'inline-block',
-                height: size, width: size,
-                backgroundColor: color}}
-          />
+    <div id={'toolbar'}>
+      <div id={'colorBar'}>
+        {colors.map(color => (<div
+          key={color}
+          onClick={props.changeColor}
+          style={{display: 'inline-block',
+                  height: swatchSize, width: swatchSize,
+                  backgroundColor: color}}
+            />)
         )}
-      <button onClick={props.clearCanvas}> CLEAR </button>
+      </div>
+      <div id={'toolButtons'}>
+        <button onClick={props.clearCanvas}> CLEAR </button>
+        <button className={props.erase} onClick={props.toggleEraser}> ERASER </button>
+      </div>
+      <div id={'brushes'}>
+        {brushSizes.map((brush, i) => (<button
+          className={(props.selectedSize === brush + '') ? 'selectedBrush' : 'brush'}
+          key={brush}
+          onClick={props.changeBrushSize}
+          value={brush}>
+            {i + 1}
+          </button>)
+        )}
+      </div>
     </div>
-  )
+  );
 }
